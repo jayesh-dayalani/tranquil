67caec15ad94056b593562cfe16ef5fdf787e3a9
Jayesh Dayalani <jayeshdayalanii@gmail.com>
2025-06-28 11:32:52 +0400

    application types state

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   const [message, setMessage] = useState('');
   const [error, setError] = useState('');
   // added later
-  const [applicationTypes, setApplicationTypes] = useState([]);
+  const [applicationTypes, setApplicationTypes] = useState<string[]>([]);
   const [projectTimeline, setProjectTimeline] = useState('');
 
 
